ea543d3e6186b6da580637fb1043bdc4b92b43d7
AdrianDC <radian.dc@gmail.com>
2015-05-23 14:45:12 +0200

    Shell script for boot-new.img easy flashing

---
+++ boot.img-FlashNewBoot.sh
@@ -0,0 +1,23 @@
+# boot.img missing
+if [ ! -f ./kernel/boot-new.img ]; then
+  echo "";
+  echo " [ boot-new.img missing in ./kernel/. Please Compile one... ]";
+  echo "";
+  read key;
+  exit;
+fi;
+
+# Folder cleaning
+echo "";
+echo " [ Cleaning the workspace ]";
+echo "";
+cd ./kernel/;
+sudo fastboot flash boot boot-new.img;
+sudo fastboot reboot;
+
+# Script end
+echo "";
+echo " [ Done ]";
+echo "";
+read key;
+
